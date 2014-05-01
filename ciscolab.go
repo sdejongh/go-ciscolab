46dca2246f4248830aa14c5a7f9a4d3709763ccb
Steve De Jongh <dejongh.st@gmail.com>
2014-05-01 17:53:38 +0200

    Menu package dev in progress... stuck with function mapping :-/

--- ciscolab.go
+++ ciscolab.go
@@ -2,34 +2,51 @@
 package main
 
 import (
-	"ciscolab/devices"
-	"ciscolab/lab"
+	//"ciscolab/devices"
+	//"ciscolab/lab"
+	"ciscolab/menu"
 	"fmt"
 )
 
+func foo() {
+	fmt.Println("Hello World!")
+}
+
 func main() {
+	/*
+		c := devices.NewComServer("ComServer3", 23, "192.168.0.3")
+		c2 := devices.NewComServer("ComServer1", 23, "192.168.0.1")
+		c3 := devices.NewComServer("ComServer2", 23, "192.168.0.2")
 
-	c := devices.NewComServer("ComServer3", 23, "192.168.0.3")
-	c2 := devices.NewComServer("ComServer1", 23, "192.168.0.1")
-	c3 := devices.NewComServer("ComServer2", 23, "192.168.0.2")
+		d := devices.NewLabDevice("P1R1", 33, c)
+		d2 := devices.NewLabDevice("P1R3", 35, c)
+		d3 := devices.NewLabDevice("P1R2", 34, c2)
 
-	d := devices.NewLabDevice("P1R1", 33, c)
-	d2 := devices.NewLabDevice("P1R3", 35, c)
-	d3 := devices.NewLabDevice("P1R2", 34, c2)
+		l := lab.NewLab()
+		l.AddDevice(d, d2, d3, c, c2, c3)
+		l.RemoveDevice("P1R2")
 
-	l := lab.NewLab()
-	l.AddDevice(d, d2, d3, c, c2, c3)
-	l.RemoveDevice("P1R2")
+		fmt.Println("CiscoLab GO version")
 
-	fmt.Println("CiscoLab GO version")
+		fmt.Println(c.GetName(), "->", c.GetIpAddress(), ":", c.GetTelnetPort())
+		fmt.Println(d.GetName(), "->", d.GetComServer().GetName(), ":", d.GetComServer().GetIpAddress(), ":", 2000+d.GetLine())
 
-	fmt.Println(c.GetName(), "->", c.GetIpAddress(), ":", c.GetTelnetPort())
-	fmt.Println(d.GetName(), "->", d.GetComServer().GetName(), ":", d.GetComServer().GetIpAddress(), ":", 2000+d.GetLine())
+		fmt.Println("Is P1R1 in lab ?", l.IsDeviceInLab("P1R1"))
 
-	fmt.Println("Is P1R1 in lab ?", l.IsDeviceInLab("P1R1"))
+		fmt.Println(l.GetComServerNames(true))
+		fmt.Println(l.GetLabDeviceNames(true))
 
-	fmt.Println(l.GetComServerNames(true))
-	fmt.Println(l.GetLabDeviceNames(true))
+		fmt.Println("P1R1 is at", l.GetDeviceByName("P1R1"))
+	*/
 
-	fmt.Println("P1R1 is at", l.GetDeviceByName("P1R1"))
+	m := menu.NewMenu("CiscoLab v3.0alpha - GO version", menu.BORDER, 2, 1, 1, 2)
+	m.SetWidth(80)
+	m.AddTextLine("Simple text line left aligned", menu.TXTALIGN_LEFT)
+	m.AddEmptyLine()
+	m.AddTextLine("Simple text line centered", menu.TXTALIGN_CENTER)
+	m.AddEmptyLine()
+	m.AddTextLine("Simple text line right aligned", menu.TXTALIGN_RIGHT)
+	m.AddEmptyLine()
+	m.AddCommandLine(1, "Test command", foo, menu.TXTALIGN_LEFT)
+	m.Display()
 }
--- menu/menu.go
+++ menu/menu.go
@@ -5,33 +5,55 @@ Generates and display the command-line menu
 
 package menu
 
+import (
+	"fmt"
+	"strings"
+)
+
 const (
 	// Some characters used to draw frames around menus
-	CHAR_CORNER                   = "+"
-	CHAR_HLINE                    = "-"
-	CHAR_VLINE                    = "|"
-	CHAR_SEPARATOR                = "-"
-	CHAR_EMPTY                    = " "
-	TXTALIGN_LEFT   TextAlignment = iota
-	TXTALIGN_CENTER TextAlignment = iota
-	TXTALIGN_RIGHT  TextAlignment = iota
+	LINETYPE_TEXT      LineType      = iota
+	LINETYPE_COMMAND   LineType      = iota
+	LINETYPE_SEPARATOR LineType      = iota
+	LINETYPE_EMPTY     LineType      = iota
+	LINETYPE_TITLE     LineType      = iota
+	CHAR_CORNER                      = "+"
+	CHAR_HLINE                       = "-"
+	CHAR_VLINE                       = "|"
+	CHAR_SEPARATOR                   = "-"
+	CHAR_EMPTY                       = " "
+	CHAR_NEWLINE                     = "\n"
+	BORDER                           = true
+	NOBORDER                         = false
+	TXTALIGN_LEFT      TextAlignment = iota
+	TXTALIGN_CENTER    TextAlignment = iota
+	TXTALIGN_RIGHT     TextAlignment = iota
 )
+
+// Line type definition
+type LineType int
 
 // Text alignment type definition (center/left/right)
 type TextAlignment int
 
+// MenuCommand type
+type MenuCommand struct {
+	key int
+	f   func()
+}
+
 // Generic menu line definition
 type MenuLine interface {
-	GetText()
-	GetLength()
-	GetAlignment()
+	GetText() string
+	GetLength() int
+	GetAlignment() TextAlignment
+	GetType() LineType
 }
 
 // Command line definition
 type CommandLine struct {
-	key     int
 	text    string
-	command func()
+	command MenuCommand
 	align   TextAlignment
 }
 
@@ -40,6 +62,9 @@ type TextLine struct {
 	content string
 	align   TextAlignment
 }
+
+// Title line definition
+type TitleLine string
 
 // Separator line definition
 type SeparatorLine string
@@ -50,6 +75,7 @@ type EmptyLine string
 // Menu type definition
 type Menu struct {
 	content   []MenuLine
+	commands  map[int]CommandLine
 	width     int
 	border    bool
 	padding_x int
@@ -58,14 +84,99 @@ type Menu struct {
 	margin_y  int
 }
 
+/**************************/
 /* MENU RELATED FUNCTIONS */
+/**************************/
+
+// Menu Type constructor
+func NewMenu(title string, border bool, padding_x int, padding_y int, margin_x int, margin_y int) *Menu {
+	menu := Menu{width: 0, border: border, padding_x: padding_x, padding_y: padding_y, margin_x: margin_x, margin_y: margin_y}
+	menu.SetTitle(title)
+	return &menu
+}
 
 // Prints the menu to the standard output
-func (m Menu) Display() {}
+func (m Menu) Display() {
+	menuWidth := m.GetWidth()
+	var emptyLine EmptyLine
+	hline := strings.Repeat(CHAR_EMPTY, m.margin_x) + CHAR_CORNER + strings.Repeat(CHAR_HLINE, menuWidth) + CHAR_CORNER
 
-// Returns menu width
+	// Print Vertical margin
+	for i := 0; i < m.margin_y; i++ {
+		fmt.Println(CHAR_EMPTY)
+	}
+
+	// Print menu top border if needed
+	if m.border {
+		fmt.Println(hline)
+	}
+
+	// Prints vertical padding (space between border and next line)
+	for i := 0; i < m.GetPaddingY(); i++ {
+		fmt.Println(m.FormatString(emptyLine))
+	}
+
+	// Print each line added to the menu
+	for _, line := range m.content {
+		fmt.Println(m.FormatString(line))
+		switch line.GetType() {
+		case LINETYPE_TITLE:
+			for i := 0; i < m.GetPaddingY(); i++ {
+				fmt.Println(m.FormatString(emptyLine))
+			}
+			if m.border {
+				fmt.Println(hline)
+			}
+			for i := 0; i < m.GetPaddingY(); i++ {
+				fmt.Println(m.FormatString(emptyLine))
+			}
+		}
+	}
+
+	// Prints vertical padding
+	for i := 0; i < m.GetPaddingY(); i++ {
+		fmt.Println(m.FormatString(emptyLine))
+	}
+
+	// Print menu bottom border if needed
+	if m.border {
+		fmt.Println(hline)
+	}
+
+}
+
+// Returns formated string including padding, border etc...
+func (m Menu) FormatString(line MenuLine) string {
+	menuWidth := m.GetWidth()
+	emptyChars := menuWidth - line.GetLength()
+	formatedString := ""
+	switch line.GetAlignment() {
+	case TXTALIGN_LEFT:
+		formatedString = strings.Repeat(CHAR_EMPTY, m.padding_x) + line.GetText() + strings.Repeat(CHAR_EMPTY, emptyChars-m.padding_x)
+	case TXTALIGN_CENTER:
+		formatedString = strings.Repeat(CHAR_EMPTY, int(emptyChars/2)) + line.GetText()
+		formatedString += strings.Repeat(CHAR_EMPTY, menuWidth-len(formatedString))
+	case TXTALIGN_RIGHT:
+		formatedString = strings.Repeat(CHAR_EMPTY, emptyChars-m.padding_x) + line.GetText() + strings.Repeat(CHAR_EMPTY, m.padding_x)
+	}
+	if m.border {
+		formatedString = CHAR_VLINE + formatedString + CHAR_VLINE
+	}
+	formatedString = strings.Repeat(CHAR_EMPTY, m.margin_x) + formatedString
+	return formatedString
+}
+
+// Returns menu width (including padding etc., but without border)
 func (m Menu) GetWidth() int {
-	return m.width
+	maxLength := m.width
+	for _, line := range m.content {
+		length := line.GetLength()
+		if length > maxLength {
+			maxLength = length
+		}
+	}
+	maxLength += 2 * m.GetPaddingX()
+	return maxLength
 }
 
 // Sets menu width
@@ -113,11 +224,44 @@ func (m *Menu) SetMarginY(y int) {
 	m.margin_y = y
 }
 
-/* TEXTLINE RELATED FUNCTIONS */
+// Set TitleLine of the Menu
+func (m *Menu) SetTitle(title string) {
+	newline := TitleLine(title)
+	m.content = append(m.content, newline)
+}
 
-// Returns TextLine text contend
+// Add a TextLine to the Menu
+func (m *Menu) AddTextLine(text string, align TextAlignment) {
+	newline := TextLine{content: text, align: align}
+	m.content = append(m.content, newline)
+}
+
+// Add a blank line to the Menu
+func (m *Menu) AddEmptyLine() {
+	var newline EmptyLine
+	m.content = append(m.content, newline)
+}
+
+// Add a command line to the Menu
+func (m *Menu) AddCommandLine(key int, text string, command func(), align TextAlignment) {
+	newcmd := MenuCommand{key: key, f: command}
+	newline := CommandLine{text: text, command: newcmd}
+	m.content = append(m.content, newline)
+	m.commands[key] = newline
+}
+
+/******************************/
+/* TEXTLINE RELATED FUNCTIONS */
+/******************************/
+
+// Returns LineType LINETYPE_TEXT
+func (t TextLine) GetType() LineType {
+	return LINETYPE_TEXT
+}
+
+// Returns TextLine text content
 func (t TextLine) GetText() string {
-	return t.GetText()
+	return t.content
 }
 
 // Returns TextLine Alignment
@@ -130,24 +274,67 @@ func (t TextLine) GetLength() int {
 	return len(t.content)
 }
 
+/*******************************/
+/* TITLELINE RELATED FUNCTIONS */
+/*******************************/
+
+// Returns LineType LINETYPE_TITLE
+func (t TitleLine) GetType() LineType {
+	return LINETYPE_TITLE
+}
+
+// Returns TitleLine text content
+func (t TitleLine) GetText() string {
+	return string(t)
+}
+
+// Returns TitleLine Alignment
+func (t TitleLine) GetAlignment() TextAlignment {
+	return TXTALIGN_CENTER
+}
+
+// Returns Textline text length
+func (t TitleLine) GetLength() int {
+	return len(t)
+}
+
+/*********************************/
 /* COMMANDLINE RELATED FUNCTIONS */
+/*********************************/
+
+// Returns LineType LINETYPE_COMMAND
+func (c CommandLine) GetType() LineType {
+	return LINETYPE_COMMAND
+}
 
 // Returns CommandLine text description
 func (c CommandLine) GetText() string {
-	return c.GetText()
+	return fmt.Sprintf("%d %q %s", c.command.key, CHAR_SEPARATOR, c.text)
 }
 
 // Returns CommandLine text length
-func (t CommandLine) GetLength() int {
-	return len(t.text)
+func (c CommandLine) GetLength() int {
+	return len(c.GetText())
 }
 
 // Returns CommandLine text alignment
-func (t CommandLine) GetAlignment() TextAlignment {
-	return t.align
+func (c CommandLine) GetAlignment() TextAlignment {
+	return c.align
 }
 
+// Returns CommandLine key
+func (c CommandLine) GetKey() int {
+	return c.command.key
+}
+
+/************************************/
 /* SEPARATOR LINE RELATED FUNCTIONS */
+/************************************/
+
+// Returns LineType LINETYPE_SEPARATOR
+func (s SeparatorLine) GetType() LineType {
+	return LINETYPE_SEPARATOR
+}
 
 // Returns a single separator character
 func (s SeparatorLine) GetText() string {
@@ -164,7 +351,14 @@ func (s SeparatorLine) GetAlignment() TextAlignment {
 	return TXTALIGN_CENTER
 }
 
+/********************************/
 /* EMPTY LINE RELATED FUNCTIONS */
+/********************************/
+
+// Returns LineType LINETYPE_EMPTY
+func (e EmptyLine) GetType() LineType {
+	return LINETYPE_EMPTY
+}
 
 // Returns a single space character
 func (e EmptyLine) GetText() string {
